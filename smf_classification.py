2b7389ebc4b4c412bd67e18175608a1cca09ecf3
Dinesh Kumar T <76119910+Dineshkumar673@users.noreply.github.com>
2021-10-25 06:51:57 +0530

    Add files via upload

---
+++ smf_classification.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#first performing ravel and then appending it
+print("$$$Script Started$$$")
+import numpy as np
+import os
+x=[]
+y=[]
+for i in os.listdir('/mnt/fs/Splitted_data/train/'):    
+    file_names=os.listdir(os.path.join('/mnt/fs/Splitted_data/train/',i))
+    for k in file_names:
+        values=np.load(os.path.join('/mnt/fs/Splitted_data/train/',i,k))
+        flattened_val = values.ravel()
+        #values.reshape(1,-1)
+        #print(len(flattened_val))
+        x.append(flattened_val)
+        y.append(i)
+print("$$$$Done with appending Train values$$$")
+
+
+# In[3]:
+
+
+X_train =np.array(x)
+Y_train =np.array(y)
+print("$$$Coverted to array$$$")
+
+
+# In[5]:
+
+
+#first performing ravel and then appending it
+import numpy as np
+import os
+x=[]
+y=[]
+for i in os.listdir('/mnt/fs/Splitted_data/test/'):    
+    file_names=os.listdir(os.path.join('/mnt/fs/Splitted_data/test/',i))
+    for k in file_names:
+        values=np.load(os.path.join('/mnt/fs/Splitted_data/test/',i,k))
+        flattened_val = values.ravel()
+        #values.reshape(1,-1)
+        #print(len(flattened_val))
+        x.append(flattened_val)
+        y.append(i)
+print("$$$$Done with appending Test values$$$")
+
+
+# In[7]:
+
+
+X_test =np.array(x)
+Y_test =np.array(y)
+print("$$$Coverted to array$$$")
+
+
+# In[8]:
+
+
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+# In[13]:
+
+
+#Knn algorithm
+# NOW WITH K=5
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train,Y_train)
+pred = knn.predict(X_test)
+print("$$$$KNN ALGORITHM COMPLETED$$$$")
+
+
+# In[14]:
+
+
+#Accuracy knn
+from sklearn.metrics import confusion_matrix, accuracy_score
+matrix= confusion_matrix(Y_test, pred)
+score = accuracy_score(Y_test, pred)
+print(matrix)
+print('Accuracy Score :',score)
+print("$$$$Done with Accuracy of Knn$$$$")
+
+
+# In[15]:
+
+
+#Randomforest algorithm
+rf = RandomForestClassifier()
+rf.fit(X_train,Y_train)
+pred1 = rf.predict(X_test)
+print("$$$$ RandomForest Completed $$$$$$$")
+
+
+# In[16]:
+
+
+#Accuracy randomforest
+matrix= confusion_matrix(Y_test, pred1)
+score = accuracy_score(Y_test, pred1)
+print(matrix)
+print('Accuracy Score :',score)
+print("$$$Done with Accuracy of RandomForest$$$")
+
+
+# In[18]:
+
+
+from xgboost import XGBClassifier
+#xgboost algorithm
+xg = XGBClassifier()
+xg.fit(X_train,Y_train)
+pred2 = xg.predict(X_test)
+print("$$$XGBoost Completed$$$$")
+
+
+# In[19]:
+
+
+#Accuracy xgboost
+matrix= confusion_matrix(Y_test, pred2)
+score = accuracy_score(Y_test, pred2)
+print(matrix)
+print('Accuracy Score :',score)
+print("$$$Done with Accuracy of XGboost$$$$")
+
+
+# In[20]:
+
+
+import pickle
+with open('knn_algo.pkl', 'wb') as f:
+    pickle.dump(knn, f)
+print("$$$Done with dumping knn model$$$$")
+
+# In[21]:
+
+
+import pickle
+with open('randomforest_algo.pkl', 'wb') as f:
+    pickle.dump(rf, f)
+print("$$$Done with dumping rf model$$$$")
+
+# In[25]:
+
+
+import pickle
+with open('xgboost_algo.pkl', 'wb') as f:
+    pickle.dump(xg, f)
+print("$$$$PROGRAM COMPLETED$$$$")
+
+
+# In[23]:
+
+
+
+
