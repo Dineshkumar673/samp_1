7a6c1e071df9f6ace60ffa1383f62d313a8bfe49
Dinesh Kumar T <76119910+Dineshkumar673@users.noreply.github.com>
2021-10-27 11:28:32 +0530

    Add files via upload

---
+++ randomforest.py
@@ -0,0 +1,49 @@
+
+
+print("$$$Script Started$$$")
+#first performing ravel and then appending it
+import numpy as np
+import os
+x=[]
+y=[]
+for i in os.listdir('/mnt/fs/Splitted_data/train/'):    
+    file_names=os.listdir(os.path.join('/mnt/fs/Splitted_data/train/',i))
+    for k in file_names:
+        values=np.load(os.path.join('/mnt/fs/Splitted_data/train/',i,k))
+        flattened_val = values.ravel()
+        #values.reshape(1,-1)
+        #print(len(flattened_val))
+        x.append(flattened_val)
+        y.append(i)
+        print('in progress')
+print("$$$Done with Appending train values$$$")        
+
+
+# In[ ]:
+
+
+X_train =np.array(x)
+Y_train =np.array(y)
+print("$$$Train values Converted to array$$$")
+
+
+
+print("$$$Loading sklearn packages$$$")
+from sklearn.ensemble import RandomForestClassifier
+
+
+# In[ ]:
+
+
+print("$$$Fitting Random Forest Model$$$")
+rf = RandomForestClassifier()
+rf.fit(X_train,Y_train)
+print("$$$RANDOM FOREST ALGORITHM COMPLETED$$$")
+
+
+
+print("$$$Dumping Model$$$")
+import pickle
+with open('randomforest.pkl', 'wb') as f:
+    pickle.dump(rf, f)
+print("$$$Script Done$$$")
