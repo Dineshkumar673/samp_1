d2c9befd193f44b5dffa036b3c1e86dd22a994c9
Dinesh Kumar T <76119910+Dineshkumar673@users.noreply.github.com>
2021-10-29 10:22:06 +0530

    Add files via upload

---
+++ testing_rf.py
@@ -0,0 +1,56 @@
+
+
+print("$$$Starting with test values$$$")
+#first performing ravel and then appending it
+import numpy as np
+import os
+x=[]
+y=[]
+for i in os.listdir('/mnt/fs/Splitted_data/test/'):    
+    file_names=os.listdir(os.path.join('/mnt/fs/Splitted_data/test/',i))
+    for k in file_names:
+        values=np.load(os.path.join('/mnt/fs/Splitted_data/test/',i,k))
+        flattened_val = values.ravel()
+        #values.reshape(1,-1)
+        #print(len(flattened_val))
+        x.append(flattened_val)
+        y.append(i)
+print("$$$Done with Appending test values$$$")   
+
+
+# In[5]:
+
+
+X_test =np.array(x)
+Y_test =np.array(y)
+print("$$$Test values Converted to array$$$")
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+print("$$$Script started$$$")
+import pickle 
+pickled_model = pickle.load(open('/mnt/fs/Splitted_data/samp_1/randomforest.pkl', 'rb'))
+print("$$$Model loaded$$$")
+pred = pickled_model.predict(X_test)
+print("$$$Measuring Accuracy of randomforest$$$")
+#Accuracy knn
+from sklearn.metrics import confusion_matrix, accuracy_score
+matrix= confusion_matrix(Y_test, pred)
+score = accuracy_score(Y_test, pred)
+print("$$$Printing Confusion matrix$$$")
+#print(matrix)
+print("$$$Printing Accuracy$$$")
+print('Accuracy Score :',score)
+print("$$$Done with Accuracy of Knnrandomforest$$$")
+print("$$$Script Done$$$")
+import pandas as pd
+import csv
+pd.DataFrame(matrix).to_csv("/mnt/fs/Splitted_data/samp_1/con_mat_rf.csv")
