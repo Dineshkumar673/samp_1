9bb05e02a230f603f88b9e256d6264d40011cab3
Dinesh Kumar T <76119910+Dineshkumar673@users.noreply.github.com>
2021-11-10 15:01:47 +0530

    Update copy.py

--- copy.py
+++ copy.py
@@ -10,7 +10,7 @@ dir_name='/mnt/fs/Synthetic_dataset_creation/Absdiff_dataset/Datasets/Absdiff_ba
 dir_val=os.listdir(dir_name)
 #print(dir_val)
 classes_dir = ['after-30', 'afternoon-36', 'age-36',  'any-35',
-                   'baby-34', 'brother-35', 'birthday-34','eat-34', 'sorry-31', 'sister-28']    
+                   'baby-34', 'brother-35', 'birthday-34','eat-34', 'sorry-31']    
 dest='/mnt/fs/Splitted_data/Arya_chudidhar_0002'
 for i in dir_val:
     os.mkdir(dest+'/'+i+'/')
