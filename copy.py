2493c944a46f6558062774a36766206c81ed169e
Dinesh Kumar T <76119910+Dineshkumar673@users.noreply.github.com>
2021-11-10 15:35:41 +0530

    Update copy.py

--- copy.py
+++ copy.py
@@ -6,12 +6,12 @@
 
 import os
 import shutil
-dir_name='/mnt/fs/Synthetic_dataset_creation/Absdiff_dataset/Datasets/Absdiff_base_dir/segmented_dataset/Aryan_augs_0000/'
+dir_name='/mnt/fs/Synthetic_dataset_creation/Absdiff_dataset/Datasets/Absdiff_base_dir/segmented_dataset/Aryan_augs_0001/'
 dir_val=os.listdir(dir_name)
 #print(dir_val)
 classes_dir = ['address-31','after-30', 'afternoon-36', 'age-36',  'any-35',
                    'baby-34', 'brother-35', 'birthday-34','eat-34']    
-dest='/mnt/fs/Splitted_data/Aryan_0000'
+dest='/mnt/fs/Splitted_data/Aryan_0001'
 for i in dir_val:
     os.mkdir(dest+'/'+i+'/')
     for j in classes_dir:
