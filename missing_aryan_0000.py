03212a4c9c21208184e9b7e826dba70ea1222183
Dinesh Kumar T <76119910+Dineshkumar673@users.noreply.github.com>
2021-11-19 08:52:50 +0530

    Update and rename missing_chudi_0003.py to missing_aryan_0000.py

--- missing_aryan_0000.py
+++ missing_aryan_0000.py
@@ -19,12 +19,12 @@ from mediapipe.python.solutions import hands as mp_hands
 # In[ ]:
 
 
-path = '/mnt/fs/Synthetic_dataset_creation/Absdiff_dataset/Datasets/Absdiff_base_dir/segmented_dataset/Arya_chudidhar_augs_0003/'
+path = '/mnt/fs/Synthetic_dataset_creation/Absdiff_dataset/Datasets/Absdiff_base_dir/segmented_dataset/Aryan_augs_0000/'
 #The immediate file path
 
 directory_contents = os.listdir(path)
 print(directory_contents)
-dest='/mnt/fs/Synthetic_dataset_creation/Absdiff_dataset/Datasets/Absdiff_base_dir/Arya_chudi_missing/Arya_chudidhar_0003'
+dest='/mnt/fs/Synthetic_dataset_creation/Absdiff_dataset/Datasets/Absdiff_base_dir/Aryan_missing/Aryan_0000/'
 
 
 # In[ ]:
@@ -231,5 +231,5 @@ for i in range (0,len(directory_contents)):
                     pinky_tip_x = hand_landmark.landmark[20].x
                     pinky_tip_y = hand_landmark.landmark[20].y
                     
-print("$$$Done With Arya_chudidhar_0003")
+print("$$$Done With Aryan_0000")
 
